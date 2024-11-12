64b3e2207ba747507ee63c006a9064ce5b77fcef
SkSohelPasha <sohelpasha019@gmail.com>
2024-11-12 21:14:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+import React, { Component } from 'react'
+import Navbar from './components/NavBar';
+import News from './components/News';
+import LoadingBar from 'react-top-loading-bar'
+
+// introduction to classes in reactjs
+export default class App extends Component {
+  pageSize=8;
+  state={
+    progress:0
+  }
+
+setProgress=(progress)=>{
+  this.setState({progress:progress})
+}
+
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <div>
+      <LoadingBar
+      height={3}
+        color='#f11946'
+        progress={this.state.progress}
+
+      />
+      </div>
+        <News setProgress={this.setProgress} pageSize={this.pageSize} country='us' category="play"/>
+
+      </div>
+    )
+  }
+}
